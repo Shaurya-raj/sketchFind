3b695e27e87c2b5424ef51583d4a1821e008fff0
Shaurya-raj <84217871+Shaurya-raj@users.noreply.github.com>
2022-07-02 10:52:23 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,10 +1,40 @@
+function preload(){
+    classifier=ml5.imageClassifier("DoodleNet");
+}
+
 function setup(){
     canvas=createCanvas(300,300);
     canvas.center();
     background("white")
+    canvas.mouseReleased(classifyCanvas);
+    synth=window.speechSynthesis;
 }
 
 function clearCanvas(){
     background("white");
 }
 
+function draw(){
+    strokeWeight(13);
+    stroke("red");
+    if(mouseIsPressed){
+        line(pmouseX,pmouseY,mouseX,mouseY);
+    }
+}
+
+function classifyCanvas(){
+    classifier.classify(canvas,gotResult);
+}
+
+function gotResult(error,results){
+    if(error){
+        console.log(error);
+    }
+    else{
+        console.log(results);
+        document.getElementById("label").innerHTML=results[0].label;
+        document.getElementById("confidence").innerHTML=Math.round(results[0].confidence*100)+"%";
+        utter=new SpeechSynthesisUtterance(results[0].label);
+        synth.speak(utter);
+    }
+}
